079985d1df78f19da5427be0276cbb9d39315cda
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-22 16:58:39 +0400

    Reset program

--- main.cpp
+++ main.cpp
@@ -2,38 +2,10 @@
 
 using namespace std;
 
-bool armstrong(int);
 int main()
 {
-    for (int number = 100; number < 10000; number++)
-    {
-        if (armstrong(number))
-        {
-            cout << number << " is an Armstrong number" << endl;
-        }
-    }
+    
 
     return 0;
 }
 
-bool armstrong(int num)
-{
-    int x = num;
-    int sum = 0;
-    int digit;
-    while (num > 0)
-    {
-        digit = num % 10;
-        sum += digit * digit * digit;
-        num /= 10;
-    }
-
-    if (sum == x)
-    {
-        return true;
-    }
-    else
-    {
-    return false;
-    }
-}
